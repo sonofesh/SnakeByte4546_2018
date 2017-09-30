47a26533547978de151b21434db928c3224f0ce6
ColombiaRay <30420525+ColombiaRay@users.noreply.github.com>
2017-09-30 14:50:45 -0500

    Motor fix

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrainTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrainTest.java
@@ -7,31 +7,30 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 /**
  * Created by raymo on 9/27/2017.
  */
-@Autonomous(name = "ProteinAuto")
+
+
+@Autonomous(name = "ProteinIsMyAuto")
 public class DriveTrainTest extends DriveTrain {
     @Override
     public void runOpMode() throws InterruptedException {
         initialize();
-        long currentTime = System.currentTimeMillis();
-        while (currentTime < 2000){
-            currentTime = System.currentTimeMillis();
-            moveForward(1);
-        }
+       /* moveForward(1);
+        sleep(800);
         setZero();
-        while (currentTime < 4000){
-            currentTime = System.currentTimeMillis();
-            turn(1);
-        }
+        turn(1);
+        sleep(700);
         setZero();
-        while (currentTime < 6000){
-            currentTime = System.currentTimeMillis();
-            turn(-1);
-        }
+        moveForward(1);
+        sleep(400);
         setZero();
-        while (currentTime < 8000){
-            currentTime = System.currentTimeMillis();
-            moveForward(-1);
-        }
+        moveForward(-1);
+        sleep(400);
+        setZero();*/
+        setPower(0.5, 0, calculateStrafe(0.5, 45);
+        sleep(1000);
         setZero();
+
+       ))
+
     }
 }
